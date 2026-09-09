e1bac50ebcd198c2c8138635eef2f663d90a0f82
Aleksey Shvariov <alxsshv@gmail.com>
2026-09-09 13:35:31 +0300

    Initial commit

---
+++ payment-core/src/main/java/com/github/alxsshv/paymentplatform/PaymentplatformApplication.java
@@ -0,0 +1,13 @@
+package com.github.alxsshv.paymentplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentplatformApplication.class, args);
+	}
+
+}
---
+++ payment-core/src/test/java/com/github/alxsshv/paymentplatform/PaymentplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alxsshv.paymentplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
